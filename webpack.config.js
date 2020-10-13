a935f79edaa51aca2dab1900dbb95de52a3b2848
tombenezra <tombe@wix.com>
2020-10-13 12:48:39 +0300

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+	mode: 'development',
+	entry: {
+		app: './src/index.jsx',
+		'editor.worker': 'monaco-editor/esm/vs/editor/editor.worker.js',
+		'json.worker': 'monaco-editor/esm/vs/language/json/json.worker',
+		'css.worker': 'monaco-editor/esm/vs/language/css/css.worker',
+		'html.worker': 'monaco-editor/esm/vs/language/html/html.worker',
+		'ts.worker': 'monaco-editor/esm/vs/language/typescript/ts.worker'
+	},
+	devServer: {
+		hot: true
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx']
+	},
+	output: {
+		globalObject: 'self',
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: require.resolve('babel-loader'),
+						options: {
+							presets: ['@babel/preset-env', '@babel/preset-react']
+						}
+					}
+				]
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.ttf$/,
+				use: ['file-loader']
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: 'src/index.html'
+		})
+	].filter(Boolean)
+};
